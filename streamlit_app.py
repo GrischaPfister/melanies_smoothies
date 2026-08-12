6fb44618c7688c70b5740a5fc39f7b0196b7064e
Grischa Pfister <118198455+GrischaPfister@users.noreply.github.com>
2026-08-12 15:08:58 +0200

    Comment out dataframe display and add conversion to pandas
    Comment out dataframe display and convert to pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,12 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('the name of your smoothie will be: ', name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# convert snowpark dataframe to a pandas dataframe so we can use the loc() function
+pd_df = my_dataframe.to_pandas()
+stdataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect('choose up to 5 ingredients',my_dataframe,max_selections=5)
