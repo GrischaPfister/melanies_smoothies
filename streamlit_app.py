3295267c7889cf57db912a4016aff9c2cab54f73
Grischa Pfister <118198455+GrischaPfister@users.noreply.github.com>
2026-08-12 15:10:04 +0200

    Fix typo in Streamlit dataframe function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert snowpark dataframe to a pandas dataframe so we can use the loc() function
 pd_df = my_dataframe.to_pandas()
-stdataframe(pd_df)
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect('choose up to 5 ingredients',my_dataframe,max_selections=5)
